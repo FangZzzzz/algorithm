68729fde4a55d4197d86a78b676900796f86c705
fufz <fufz@19pay.com.cn>
2020-05-14 17:14:23 +0800

    [feature] 重刷三数之和

--- src/main/java/com/cheerfun/leetcode/editor/cn/ThreeSum .java
+++ src/main/java/com/cheerfun/leetcode/editor/cn/ThreeSum .java
@@ -21,42 +21,40 @@ package com.cheerfun.leetcode.editor.cn;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 class ThreeSum {
     public static void main(String[] args) {
         Solution solution = new ThreeSum().new Solution();
-        solution.threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+        solution.threeSum(new int[]{0,0,0});
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public List<List<Integer>> threeSum(int[] nums) {
-            List<List<Integer>> result = new ArrayList<>();
-            if (nums == null || nums.length < 3) {
+            List<List<Integer>> result = new LinkedList<>();
+            if(nums == null || nums.length < 3) {
                 return result;
             }
-            Arrays.sort(nums);
-            for (int i = 0; i < nums.length - 2; i++) {
-                if (nums[i] > 0) {
-                    break;
-                }
+            quickSort(nums, 0, nums.length - 1);
+            for(int i = 0; i < nums.length - 2; i ++) {
                 if (i > 0 && nums[i] == nums[i - 1]) {
                     continue;
                 }
                 int l = i + 1, r = nums.length - 1;
-                while (l < r) {
+                while(l < r) {
                     int sum = nums[i] + nums[l] + nums[r];
                     if (sum == 0) {
                         result.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                        while (l < r && nums[l] == nums[l + 1]) {
-                            l++;
+                        while(l < r && nums[l] == nums[l+1]) {
+                            l ++;
                         }
-                        while (l < r && nums[r] == nums[r - 1]) {
-                            r--;
+                        while(l < r && nums[r] == nums[r-1]) {
+                            r --;
                         }
-                        l++;
-                        r--;
+                        l ++;
+                        r --;
                     } else if (sum > 0) {
                         r--;
                     } else {
@@ -67,20 +65,21 @@ class ThreeSum {
             return result;
         }
 
-        private void quickSort(int nums[], int start, int end) {
+
+        private void quickSort(int[] nums, int start, int end) {
             if (start >= end) {
-                return;
+                return ;
             }
-            int l = start, r = end;
             int point = nums[start];
+            int l = start, r = end;
             while (l < r) {
-                while (point < nums[r] && l < r) {
+                while (l < r && point <= nums[r]) {
                     r--;
                 }
                 if (l < r) {
                     nums[l++] = nums[r];
                 }
-                while (point > nums[l] && l < r) {
+                while (l < r && point >= nums[l]) {
                     l++;
                 }
                 if (l < r) {
