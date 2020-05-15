df1b77491c920ffdca66d7754a7cde7158a98d02
fufz <fufz@19pay.com.cn>
2020-05-15 13:37:39 +0800

    [feature] 删除链表倒数第n个节点

---
+++ src/main/java/com/cheerfun/leetcode/editor/cn/ListNode.java
@@ -0,0 +1,18 @@
+package com.cheerfun.leetcode.editor.cn;
+
+/**
+ *  * @Description TODO
+ *  * @Classname ListNode
+ *  * @Author fangzhou
+ *  * @Date 2020/5/15 1:23 下午
+ *  * @Version V1.0
+ *  
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
