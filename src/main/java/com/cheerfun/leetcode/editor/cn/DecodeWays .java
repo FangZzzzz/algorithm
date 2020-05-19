918194d7e2cbc34831297fc777806514d0b1a45b
fufz <fufz@19pay.com.cn>
2020-05-19 16:39:25 +0800

    [feature] 字符串解码 dp

---
+++ src/main/java/com/cheerfun/leetcode/editor/cn/DecodeWays .java
@@ -0,0 +1,59 @@
+//一条包含字母 A-Z 的消息通过以下方式进行了编码： 
+//
+// 'A' -> 1
+//'B' -> 2
+//...
+//'Z' -> 26
+// 
+//
+// 给定一个只包含数字的非空字符串，请计算解码方法的总数。 
+//
+// 示例 1: 
+//
+// 输入: "12"
+//输出: 2
+//解释: 它可以解码为 "AB"（1 2）或者 "L"（12）。
+// 
+//
+// 示例 2: 
+//
+// 输入: "226"
+//输出: 3
+//解释: 它可以解码为 "BZ" (2 26), "VF" (22 6), 或者 "BBF" (2 2 6) 。
+// 
+// Related Topics 字符串 动态规划
+
+package com.cheerfun.leetcode.editor.cn;
+
+class DecodeWays {
+    public static void main(String[] args) {
+        Solution solution = new DecodeWays().new Solution();
+        solution.numDecodings("27");
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int numDecodings(String s) {
+            if (s == null || s.length() == 0 || s.charAt(0) == '0') {
+                return 0;
+            }
+            int pre = 1, cur = 1;
+            for (int i = 1; i < s.length(); i++) {
+                int tmp = cur;
+                if (s.charAt(i) == '0') {
+                    if (s.charAt(i - 1) == '1' || s.charAt(i - 1) == '2') {
+                        cur = pre;
+                    } else {
+                        return 0;
+                    }
+                } else if (s.charAt(i - 1) == '1' || s.charAt(i - 1) == '2' && s.charAt(i) >= '1' && s.charAt(i) <= '6') {
+                    cur = cur + pre;
+                }
+                pre = tmp;
+            }
+            return cur;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
