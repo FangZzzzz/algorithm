28ae60a90cb9b174673a531a001c6fd0f2d67203
fufz <fufz@19pay.com.cn>
2020-05-25 14:00:38 +0800

    [feature] 给定一个二叉树，返回它的中序 遍历。

---
+++ src/main/java/com/cheerfun/leetcode/editor/cn/BinaryTreeInorderTraversal .java
@@ -0,0 +1,74 @@
+//给定一个二叉树，返回它的中序 遍历。 
+//
+// 示例: 
+//
+// 输入: [1,null,2,3]
+//   1
+//    \
+//     2
+//    /
+//   3
+//
+//输出: [1,3,2] 
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+// Related Topics 栈 树 哈希表
+
+package com.cheerfun.leetcode.editor.cn;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+class BinaryTreeInorderTraversal {
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreeInorderTraversal().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+
+    class Solution {
+
+
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> result = new LinkedList<>();
+            if (root == null) {
+                return result;
+            }
+            Stack<TreeNode> stack = new Stack<>();
+            TreeNode cur = root;
+            while (cur != null || !stack.isEmpty()) {
+                while (cur != null) {
+                    stack.push(cur);
+                    cur = cur.left;
+                }
+                cur = stack.pop();
+                result.add(cur.val);
+                cur = cur.right;
+            }
+            return result;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
