8e69a4f6ce7ce2b9372be19b8c681ef7bb22de8f
fufz <fufz@19pay.com.cn>
2020-06-05 22:12:04 +0800

    [feature] 乘积最大子数组

---
+++ src/main/java/com/cheerfun/leetcode/editor/cn/MaximumProductSubarray .java
@@ -0,0 +1,48 @@
+//给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: [2,3,-2,4]
+//输出: 6
+//解释: 子数组 [2,3] 有最大乘积 6。
+// 
+//
+// 示例 2: 
+//
+// 输入: [-2,0,-1]
+//输出: 0
+//解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。 
+// Related Topics 数组 动态规划
+
+package com.cheerfun.leetcode.editor.cn;
+
+class MaximumProductSubarray {
+    public static void main(String[] args) {
+        Solution solution = new MaximumProductSubarray().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int maxProduct(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return 0;
+            }
+            int max = nums[0], iMax = nums[0], iMin = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] < 0) {
+                    iMax += iMin;
+                    iMin = iMax - iMin;
+                    iMax -= iMin;
+                }
+                iMax = Math.max(iMax * nums[i], nums[i]);
+                iMin = Math.min(iMin * nums[i], nums[i]);
+                max = Math.max(iMax, max);
+            }
+            return max;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
