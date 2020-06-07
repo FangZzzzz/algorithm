7d9303fbc0549fded17959f182449492269d09a3
fufz <fufz@19pay.com.cn>
2020-06-07 21:41:17 +0800

    [feature] 打家劫舍3

---
+++ src/main/java/com/cheerfun/leetcode/editor/cn/TreeNode.java
@@ -0,0 +1,19 @@
+package com.cheerfun.leetcode.editor.cn;
+
+/**
+ *  * @Description TODO
+ *  * @Classname TreeNode
+ *  * @Author fangzhou
+ *  * @Date 2020/6/7 9:00 下午
+ *  * @Version V1.0
+ *  
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
