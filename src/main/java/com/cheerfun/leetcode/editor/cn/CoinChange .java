79dc8d01cfbbc1181d855ee4b877f4d144038db2
fufz <fufz@19pay.com.cn>
2020-06-17 22:09:00 +0800

    [feature] 零钱兑换

--- src/main/java/com/cheerfun/leetcode/editor/cn/CoinChange .java
+++ src/main/java/com/cheerfun/leetcode/editor/cn/CoinChange .java
@@ -39,17 +39,18 @@ class CoinChange {
             int[] dp = new int[amount + 1];
             Arrays.fill(dp, -1);
             for (int i = 0; i < coins.length; i++) {
-                if (coins[i] <= amount) {
+                if (amount >= coins[i]) {
                     dp[coins[i]] = 1;
                 }
             }
-            for (int i = 1; i <= amount; i++) {
+
+            for (int i = 0; i <= amount; i++) {
                 for (int j = 0; j < coins.length; j++) {
-                    if (coins[j] <= i && dp[i - coins[j]] != -1 ) {
-                        if (dp[i] == -1) {
+                    if (i - coins[j] > 0 && dp[i - coins[j]] != -1) {
+                        if(dp[i] == -1) {
                             dp[i] = dp[i - coins[j]] + 1;
-                        } else  {
-                            dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                        } else {
+                            dp[i] = Math.min(dp[i - coins[j]] + 1, dp[i]);
                         }
                     }
                 }
