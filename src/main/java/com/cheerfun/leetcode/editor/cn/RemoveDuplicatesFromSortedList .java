f202dc6af24a51b1e4c5940839ad44695f2b6d05
fufz <fufz@19pay.com.cn>
2020-06-20 17:37:53 +0800

    [feature] 删除排序链表中重复元素

---
+++ src/main/java/com/cheerfun/leetcode/editor/cn/RemoveDuplicatesFromSortedList .java
@@ -0,0 +1,45 @@
+//给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。 
+//
+// 示例 1: 
+//
+// 输入: 1->1->2
+//输出: 1->2
+// 
+//
+// 示例 2: 
+//
+// 输入: 1->1->2->3->3
+//输出: 1->2->3 
+// Related Topics 链表
+
+package com.cheerfun.leetcode.editor.cn;
+
+class RemoveDuplicatesFromSortedList {
+    public static void main(String[] args) {
+        Solution solution = new RemoveDuplicatesFromSortedList().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            ListNode result = head;
+            while (head != null) {
+                while (head.next != null && head.next.val == head.val) {
+                    head.next = head.next.next;
+                }
+                head = head.next;
+            }
+            return result;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
