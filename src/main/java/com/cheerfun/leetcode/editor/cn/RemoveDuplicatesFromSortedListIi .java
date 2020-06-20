cf18ce3c591dd317b2268b92b9a4b07911a54be1
fufz <fufz@19pay.com.cn>
2020-06-20 18:05:30 +0800

    [feature] 删除排序链表中的重复元素

---
+++ src/main/java/com/cheerfun/leetcode/editor/cn/RemoveDuplicatesFromSortedListIi .java
@@ -0,0 +1,68 @@
+//给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。 
+//
+// 示例 1: 
+//
+// 输入: 1->2->3->3->4->4->5
+//输出: 1->2->5
+// 
+//
+// 示例 2: 
+//
+// 输入: 1->1->1->2->3
+//输出: 2->3 
+// Related Topics 链表
+
+package com.cheerfun.leetcode.editor.cn;
+
+class RemoveDuplicatesFromSortedListIi {
+    public static void main(String[] args) {
+        Solution solution = new RemoveDuplicatesFromSortedListIi().new Solution();
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(3);
+        ListNode node5 = new ListNode(4);
+        ListNode node6 = new ListNode(4);
+        ListNode node7 = new ListNode(5);
+        ListNode node8 = new ListNode(6);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node7;
+        node7.next = node8;
+        solution.deleteDuplicates(node1);
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            ListNode hair = new ListNode(-1);
+            ListNode cur = hair;
+            hair.next = head;
+            while (cur.next != null) {
+                ListNode curHead = cur.next;
+                while (curHead != null && curHead.val == cur.next.val) {
+                    curHead = curHead.next;
+                }
+                if (curHead != cur.next.next) {
+                    cur.next = curHead;
+                } else {
+                    cur = cur.next;
+                }
+            }
+            return hair.next;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
